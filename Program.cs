b7cc0a72ef3f4d7724e1a2270af69342f6a00c15
Jacqueline Bybee <jaccbybe@ut.utm.edu>
2022-01-18 20:13:49 -0600

    Final draft

--- Program.cs
+++ Program.cs
@@ -5,8 +5,41 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   
+
+            int Ans = 0, turn = 1;
+            bool e = false;
+            Random rnd = new Random();
+            int num = rnd.Next(0, 100);
+
+            while (e == false)
+            {
+                Console.WriteLine("I am thinking of a number between 0 and 100.");
+                string v = Console.ReadLine();
+                Ans = Convert.ToInt32(v);
+
+                if (num == Ans)
+                {
+                    Console.WriteLine("YAY!\nYou are right!");
+                    Console.WriteLine("You took " + turn + " turns..");
+                    e = true;
+
+                }
+                else if (num > Ans)
+                {
+                    Console.WriteLine("Oh No...\n Too low!");
+                    e = false;
+                    turn++;
+                }
+                else if (num < Ans)
+                {
+                    Console.WriteLine("Oh No...\n Too high!");
+                    e = false;
+                    turn++;
+                }
+            }
+
         }
+        
     }
 }
